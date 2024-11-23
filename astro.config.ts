06758c35bb35dd5febf204c44a97205f1564bc14
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-11-23 18:44:18 +0100

    Add last touches

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,8 @@
-// @ts-check
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
-import sitemap from "@astrojs/sitemap";
-
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://kzuraw.com",
   integrations: [tailwind(), sitemap()],
