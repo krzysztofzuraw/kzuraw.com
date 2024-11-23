b462e2f1da407a4a0de6956536f67b7c552c2ce3
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-11-23 18:33:56 +0100

    

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async (context) => {
+  const blog = await getCollection("blog");
+
+  return rss({
+    title: "kzuraw.com",
+    description: "Website about TypeScript, React and all things frontend",
+    site: context.site?.toString() ?? "https://kzuraw.com",
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+};
