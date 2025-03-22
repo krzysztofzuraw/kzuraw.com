734c89fb63031cfedbbdadd18f3fedea5309569c
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-22 18:22:06 +0100

    add Vercel adapter and Speed Insights integration; update package dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://kzuraw.com",
@@ -9,4 +9,10 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    skewProtection: true,
+  }),
 });
